50974bf4b2d08467195b8a15ff38d579be65a311
acsb1 <acsb@cin.ufpe.br>
2020-10-26 20:36:09 -0300

    avg e std

--- Lista3/GA/gaF2.m
+++ Lista3/GA/gaF2.m
@@ -3,16 +3,29 @@ gaDat2.Objfun='objfun_F2';
 lb=[-2.048 -2.048];
 ub=[2.048 2.048];
 gaDat2.FieldD=[lb; ub];
-gaDat2.MAXGEN=10;
-gaDat2.NIND=40000; % increasing number individuals produce better solution 
-                   % but higher computational cost
+gaDat2.MAXGEN=150;
 
-gaDat2.indini= [0,0;
-                0.1,0.1;
-                -0.1,-0.1];  % insert 3 individuals in the first population
-           
-% Execute GA
-gaDat2=ga(gaDat2);
-% Result are in
-gaDat2.xmin
-gaDat2.fxmin
+v_Ind = [50, 200, 400];
+v_Pc = [0.6, 0.6, 0.7];
+v_Pm = [0.001, 0.2, 0.001];
+v = [0,0,0,0,0,0,0,0,0,0];
+avg = [0,0,0];
+des = [0,0,0];
+
+for i=1:3
+    for j=1:10
+        gaDat2.NIND= v_Ind(i);
+        gaDat2.Pc= v_Pc(i);
+        gaDat2.Pm= v_Pm(i);
+                
+        % Execute GA
+        gaDat2=ga(gaDat2);
+        % Result are in
+        gaDat2.xmin
+        gaDat2.fxmin
+        
+        v(j) = gaDat2.fxmin;
+    end
+    avg(i) = mean(v);
+    des(i) = std(v);
+end
--- Lista3/GA/gaF3.m
+++ Lista3/GA/gaF3.m
@@ -3,16 +3,29 @@ gaDat3.Objfun='objfun_F3';
 lb=[-5.12 -5.12 -5.12 -5.12 -5.12];
 ub=[5.12 5.12 5.12 5.12 5.12];
 gaDat3.FieldD=[lb; ub];
-gaDat3.MAXGEN=10;
-gaDat3.NIND=40000; % increasing number individuals produce better solution 
-                 % but higher computational cost
+gaDat3.MAXGEN=1;
 
-%gaDat3.indini= [0,0,0,0,0;
-%                0.1,0.1,0.1,0.1,0.1;
-%                -0.1,-0.1,-0.1,-0.1,-0.1]; 
-                          
-% Execute GA
-gaDat3=ga(gaDat3);
-% Result are in
-gaDat3.xmin
-gaDat3.fxmin
+v_Ind = [50, 200, 400];
+v_Pc = [0.6, 0.6, 0.7];
+v_Pm = [0.001, 0.2, 0.001];
+v = [0,0,0,0,0,0,0,0,0,0];
+avg = [0,0,0];
+des = [0,0,0];
+
+for i=1:3
+    for j=1:10
+        gaDat3.NIND= v_Ind(i);
+        gaDat3.Pc= v_Pc(i);
+        gaDat3.Pm= v_Pm(i);
+                
+        % Execute GA
+        gaDat3=ga(gaDat3);
+        % Result are in
+        gaDat3.xmin
+        gaDat3.fxmin
+        
+        v(j) = gaDat3.fxmin;
+    end
+    avg(i) = mean(v);
+    des(i) = std(v);
+end
